bf6620bdc3e5ae3f133aadcf7f2d5e0127fafec7
bin qiu <qiub-a@glodon.com>
2023-06-10 14:24:13 +0800

    fix scene import

--- src/shaders/MeshPhysicalShader.ts
+++ src/shaders/MeshPhysicalShader.ts
@@ -1,5 +1,5 @@
 import { Material } from "../materials/Material";
-import { Scene } from "../spectre";
+import { Scene } from "../core/Scene";
 import { Shader } from "./Shader";
 import * as basic from "./ShaderBasic";
 
